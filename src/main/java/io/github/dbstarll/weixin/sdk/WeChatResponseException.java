e001d108e4e706516a48b6241b897cff1c91f730
dbstarll <dbstarll@gmail.com>
2023-05-15 16:49:33 +0800

    WeChatResponseException继承ApiProtocolException

--- src/main/java/io/github/dbstarll/weixin/sdk/WeChatResponseException.java
+++ src/main/java/io/github/dbstarll/weixin/sdk/WeChatResponseException.java
@@ -1,12 +1,13 @@
 package io.github.dbstarll.weixin.sdk;
 
-import io.github.dbstarll.utils.net.api.ApiResponseException;
+import io.github.dbstarll.utils.net.api.ApiProtocolException;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.hc.client5.http.HttpResponseException;
 
-public class WeChatResponseException extends ApiResponseException {
+public class WeChatResponseException extends ApiProtocolException {
     private static final String RID_SPLIT_TOKEN = " rid: ";
 
+    private final int statusCode;
+    private final String reasonPhrase;
     private final String rid;
 
     /**
@@ -16,8 +17,28 @@ public class WeChatResponseException extends ApiResponseException {
      * @param errMsg  错误信息
      */
     public WeChatResponseException(final int errCode, final String errMsg) {
-        super(new HttpResponseException(errCode, StringUtils.substringBefore(errMsg, RID_SPLIT_TOKEN)));
+        super(null);
+        this.statusCode = errCode;
+        this.reasonPhrase = StringUtils.substringBefore(errMsg, RID_SPLIT_TOKEN);
         this.rid = StringUtils.substringAfter(errMsg, RID_SPLIT_TOKEN);
+    }
+
+    /**
+     * 获得异常的状态码.
+     *
+     * @return 状态码
+     */
+    public final int getStatusCode() {
+        return this.statusCode;
+    }
+
+    /**
+     * 获得异常的原因.
+     *
+     * @return 异常原因
+     */
+    public final String getReasonPhrase() {
+        return this.reasonPhrase;
     }
 
     /**
