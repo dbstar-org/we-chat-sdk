7796a252cf46829f8b400645cced88b6de3c269e
dbstarll <dbstarll@gmail.com>
2023-05-17 15:55:42 +0800

    使用json转化的方式来测试SessionResponse

--- src/test/java/io/github/dbstarll/weixin/sdk/SessionResponseTest.java
+++ src/test/java/io/github/dbstarll/weixin/sdk/SessionResponseTest.java
@@ -1,38 +1,34 @@
 package io.github.dbstarll.weixin.sdk;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 class SessionResponseTest {
-    @Test
-    void testToString() {
-        final SessionResponse response = new SessionResponse();
-        assertEquals("SessionResponse[openid='null', sessionKey='null', unionid='null']", response.toString());
+    private ObjectMapper mapper;
+
+    @BeforeEach
+    void setUp() {
+        mapper = new ObjectMapper().setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+    }
+
+    @AfterEach
+    void tearDown() {
+        mapper = null;
     }
 
     @Test
-    void getSessionKey() {
-        final SessionResponse response = new SessionResponse();
-        assertNull(response.getSessionKey());
-        response.setSessionKey("setSessionKey");
-        assertEquals("setSessionKey", response.getSessionKey());
-    }
-
-    @Test
-    void getUnionid() {
-        final SessionResponse response = new SessionResponse();
-        assertNull(response.getUnionid());
-        response.setUnionid("setUnionid");
-        assertEquals("setUnionid", response.getUnionid());
-    }
-
-    @Test
-    void getOpenid() {
-        final SessionResponse response = new SessionResponse();
-        assertNull(response.getOpenid());
-        response.setOpenid("setOpenid");
-        assertEquals("setOpenid", response.getOpenid());
+    void readValue() throws JsonProcessingException {
+        final String json = "{\"session_key\":\"LbQKDE/KXI47DqZQtaQZiQ==\",\"openid\":\"oM_r64gOfnupu5c1kPz1CDcD0WyA\",\"unionid\":\"wxa9b59367ce000000\"}";
+        final SessionResponse response = mapper.readValue(json, SessionResponse.class);
+        assertEquals(123, response.toString().length());
+        assertEquals("oM_r64gOfnupu5c1kPz1CDcD0WyA", response.getOpenid());
+        assertEquals("LbQKDE/KXI47DqZQtaQZiQ==", response.getSessionKey());
+        assertEquals("wxa9b59367ce000000", response.getUnionid());
     }
 }
